9c95079f8e361b5212a6e62526be4b83fa5afb63
xukai <xukaik3@gmail.com>
2021-07-15 16:59:04 +0800

    [FIX] user DDL

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -16,49 +16,49 @@ export class User {
 
   @Index()
   @Column({
-    nullable: true,
     comment: '用户名',
   })
   user_name: string;
 
   @Index()
   @Column({
-    nullable: true,
     comment: '密码',
   })
   password: string;
 
   @Column({
     type: 'tinyint',
-    nullable: true,
-    default: 2,
     comment: '昵称',
   })
   nick_name: string;
 
   @Column({
     unsigned: true,
-    nullable: true,
+    default: 2,
     comment: '性别 0:女,1:男,2:其他',
   })
   gender: number;
 
   @Column({
+    nullable: true,
     comment: '城市',
   })
   city: number;
 
   @Column({
+    nullable: true,
     comment: '头像',
   })
   avatar: number;
 
   @CreateDateColumn({
+    nullable: true,
     comment: '创建时间',
   })
   created_at: string;
 
   @UpdateDateColumn({
+    nullable: true,
     comment: '更新时间',
   })
   updated_at: string;
